df30ada5bc0a6e8d191294097231c6a7b5e4d3f8
Talha Yerlikaya <mtalhayerlikaya@gmail.com>
2024-07-26 18:42:09 +0300

    libraries added

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.navigation.safeargs)
+}
+
+android {
+    namespace = "com.example.emarketapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.emarketapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.viewmodel)
+    implementation(libs.androidx.viewmodel.ktx)
+    implementation(libs.androidx.fragment)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.fragment.ui)
+    implementation(libs.hilt)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    annotationProcessor(libs.hilt.compiler) //kapt
+    implementation(libs.google.gson)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.retrofit.converter.scalars)
+    implementation(libs.glide)
+    implementation(libs.androidx.room)
+    annotationProcessor(libs.androidx.room.compiler) // annotationProcessor
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.android.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
